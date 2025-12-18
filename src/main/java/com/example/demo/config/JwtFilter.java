1390c3c41beda707780433a9384ad8bcca296995
bala-amypo <bala@amypo.in>
2025-12-18 10:06:20 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,11 +12,12 @@ public class JwtFilter {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://9068.408procr.amypo.ai/");
+        server.setDescription("Server");
+
         return new OpenAPI()
-                // You need to change the port as per your server
-                servers = List.of(server);
-(
-                        new Server().url("https://9068.408procr.amypo.ai/")
-                ));
-        }
+                .setServers(List.of(server));
+    }
 }
