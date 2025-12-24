d0ab771fcfc284bfcef225c9c59acc9f3543c5b2
bala-amypo <bala@amypo.in>
2025-12-24 06:22:04 +0000

    User commit

--- src/main/java/com/example/demo/service/AnalysisLogService.java
+++ src/main/java/com/example/demo/service/AnalysisLogService.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.AnalysisLog;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface AnalysisLogService {
+
     AnalysisLog createLog(Long zoneId, String message);
+
     List<AnalysisLog> getLogsByZone(Long zoneId);
 }
