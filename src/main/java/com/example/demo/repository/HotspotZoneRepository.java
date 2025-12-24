29642b18082cb4a2f6048b1b59b234829809e981
bala-amypo <bala@amypo.in>
2025-12-24 04:25:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/HotspotZoneRepository.java
+++ src/main/java/com/example/demo/repository/HotspotZoneRepository.java
@@ -1,5 +1,15 @@
-public interface HotspotZoneRepository extends JpaRepository<HotspotZone, Long> {
-    boolean existsByZoneName(String zoneName);
+package com.example.demo.repository;
+
+import com.example.demo.model.HotspotZone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HotspotZoneRepository
+        extends JpaRepository<HotspotZone, Long> {
+
     Optional<HotspotZone> findByZoneName(String zoneName);
-    List<HotspotZone> findBySeverityLevel(String severityLevel);
+
+    List<HotspotZone> findBySeverityLevel(String level);
 }
