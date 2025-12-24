4e6eb0f304d29eac008d71bfa805ff2f5bf911e5
bala-amypo <bala@amypo.in>
2025-12-24 06:34:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/CrimeReportController.java
+++ src/main/java/com/example/demo/controller/CrimeReportController.java
@@ -1,32 +1,33 @@
-
 package com.example.demo.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.CrimeReport;
 import com.example.demo.service.CrimeReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+import java.util.List;
+
 @RestController
 @RequestMapping("/reports")
+@Tag(name = "Crime Reports")
 public class CrimeReportController {
 
-@Autowired
-private CrimeReportService crimeReportService;
+    private final CrimeReportService service;
 
-@PostMapping("/")
-public CrimeReport addReport(@RequestBody CrimeReport report) {
-return crimeReportService.addReport(report);
+    public CrimeReportController(CrimeReportService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @Operation(summary = "Add crime report")
+    public CrimeReport add(@RequestBody CrimeReport report) {
+        return service.addReport(report);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all crime reports")
+    public List<CrimeReport> getAll() {
+        return service.getAllReports();
+    }
 }
-
-@GetMapping("/")
-public List<CrimeReport> getAllReports() {
-return crimeReportService.getAllReports();
-}
-}
-
-
-
